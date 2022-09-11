bc1d134ddfffefede9a2998948762a33ef0be790
sponsorCoinAdmin <sponsorcoin@protonmail.com>
2022-09-11 15:44:16 -0400

    Merge Master

--- pages/Wallet/GUI_Wallet.js
+++ pages/Wallet/GUI_Wallet.js
@@ -113,7 +113,6 @@ function GUI_ClosePopupWallet(selectId) {
 }
 
 function selectedTokenChanged() {
-  alert("HERE");
   var selector = document.getElementById("tokenContract_SEL");
   var size = selector.options.length;
   var idx = selector.selectedIndex;
